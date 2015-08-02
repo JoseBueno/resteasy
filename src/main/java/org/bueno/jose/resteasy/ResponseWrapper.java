3e34440a7c5244b641d1251ef284432b9894cb41
Jose Bueno <bueno.jose@gmail.com>
2015-08-01 18:26:10 -0700

    Initial commit

---
+++ src/main/java/org/bueno/jose/resteasy/ResponseWrapper.java
@@ -0,0 +1,49 @@
+import java.lang.Exception;
+
+public class ResponseWrapper{
+
+    private int statusCode;
+    private String ResponseText;
+    private Exception exception;
+
+    public ResponseWrapper(){}
+
+    public ResponseWrapper(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public ResponseWrapper(int statusCode, String responseText) {
+        this.statusCode = statusCode;
+        ResponseText = responseText;
+    }
+
+    public ResponseWrapper(int statusCode, String responseText, Exception exception) {
+        this.statusCode = statusCode;
+        ResponseText = responseText;
+        this.exception = exception;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getResponseText() {
+        return ResponseText;
+    }
+
+    public void setResponseText(String responseText) {
+        ResponseText = responseText;
+    }
+
+    public Exception getException() {
+        return exception;
+    }
+
+    public void setException(Exception exception) {
+        this.exception = exception;
+    }
+}
